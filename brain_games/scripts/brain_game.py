d31c0cd006803770d02d0bea45b42de56234d0f3
Gorodecki <gorodecki@mail.ru>
2021-08-19 02:23:21 +0300

    commit

---
+++ brain_games/scripts/brain_game.py
@@ -0,0 +1,13 @@
+#!/usr/local/bin/ python3
+"""Main programm."""
+from brain_games.game_engine import run
+from brain_games.games import calc
+
+
+def main():
+    """Run a user calc game."""
+    run(calc)
+
+
+if __name__ == '__main__':
+    main()
